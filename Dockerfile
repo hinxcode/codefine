6add2309c68aa6b0aeb22786f0d9c57104c29091
hinx <b2765270@gmail.com>
2015-11-11 19:22:48 +0800

    protocol

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ RUN rm -fr /var/www/html
 RUN git clone https://github.com/hinxcode/codefine.git /var/www/html
 # COPY . /var/www/html
 
-EXPOSE 80
+EXPOSE 80 443
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 		<meta charset="utf-8">
 		<title>Codefine</title>
 		<link rel="stylesheet" href="css/style.css" />
-		<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-		<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 		<script src="js/circle-progress.js"></script>
 	</head>
 	<body>
